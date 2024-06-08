55eae19c93f571c50dd4c3dd9f9af7875a1ba1d1
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-06-08 11:02:07 -0400

    Initial Commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by Enrique Sarmiento on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedApp
+//
+//  Created by Enrique Sarmiento on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
