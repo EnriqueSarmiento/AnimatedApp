78770639f8d4fd0b68f211b01fb912345045fb47
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-06-08 17:02:48 -0400

    FIXED: loading states and rive animated integrated for onboarding settings

--- AnimatedApp/Views/OnboardingView.swift
+++ AnimatedApp/Views/OnboardingView.swift
@@ -24,7 +24,7 @@ struct OnboardingView: View {
          Color.shadow.opacity(showModal ? 0.4: 0).ignoresSafeArea()
          
          if showModal {
-            SignInView()
+            SignInView(showModal: $showModal)
                .transition(.move(edge: .top).combined(with: .opacity))
                .overlay{
                   Button{
--- AnimatedApp/Views/SignInView.swift
+++ AnimatedApp/Views/SignInView.swift
@@ -6,11 +6,50 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct SignInView: View {
    
+   @Binding var showModal: Bool
    @State var email: String = ""
    @State var password: String = ""
+   @State var loading: Bool = false
+   
+   
+   var check = RiveViewModel(fileName: "RiveAssets/check", stateMachineName: "State Machine 1")
+   var confetti = RiveViewModel(fileName: "RiveAssets/confetti", stateMachineName: "State Machine 1")
+   
+   private func logIn(){
+      loading = true
+      
+      if email != "" && password != "" {
+         DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            check.triggerInput("Check")
+         })
+         
+         DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+            loading = false
+            confetti.triggerInput("Trigger explosion")
+         }
+         
+         DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+            withAnimation {
+               showModal = false
+     
+            }
+         }
+         
+         
+      }else{
+         DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            check.triggerInput("Error")
+         })
+         
+         DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+            loading = false
+         }
+      }
+   }
    
    var body: some View {
       VStack(spacing: 24) {
@@ -37,15 +76,19 @@ struct SignInView: View {
                .customTextField(image: Image("Icon Lock"))
          }
          
-         Label("Sign In", systemImage: "arrow.right")
-            .customFont(.headline)
-            .padding(20)
-            .frame(maxWidth: .infinity)
-            .background(Color(hex: "F77D8E"))
-            .foregroundColor(.white)
-            .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
-            .cornerRadius(8, corners: [.topLeft])
-            .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+         Button{
+            logIn()
+         } label: {
+            Label("Sign In", systemImage: "arrow.right")
+               .customFont(.headline)
+               .padding(20)
+               .frame(maxWidth: .infinity)
+               .background(Color(hex: "F77D8E"))
+               .foregroundColor(.white)
+               .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
+               .cornerRadius(8, corners: [.topLeft])
+               .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+         }
          
          HStack {
             Rectangle().frame(height: 1).opacity(0.1)
@@ -74,9 +117,20 @@ struct SignInView: View {
                .stroke(.linearGradient(colors: [.white.opacity(0.8),.white.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
          }
          .padding()
+         .overlay{
+            if loading {
+               check.view()
+                  .frame(width: 100, height: 100)
+                  .allowsHitTesting(false)
+            }
+            
+            confetti.view()
+               .scaleEffect(3)
+               .allowsHitTesting(false)
+         }
    }
 }
 
 #Preview {
-    SignInView()
+   SignInView(showModal: .constant(true))
 }
